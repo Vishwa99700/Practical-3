7fddad1c367059357f045c31244acde4b0c5c4c8
Vishwa Sudaraka <vishwa25flash@gmail.com>
2024-10-19 15:44:30 +0530

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout << " Vishwa Sudaraka" << endl;
+    cout << " Student " << endl;
+    cout << " University Of Vavuniya " << endl;
+
+    return 0;
+}
